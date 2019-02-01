3b161d1ebe0c0c18aa469d305031dfd5a4b1ffb3
Adarsh Kumar <adarsh.kumar@thecarousell.com>
2019-02-01 20:09:39 +0800

    adding array api

--- array/array.go
+++ array/array.go
@@ -5,13 +5,51 @@ import(
 	"sort"
 )
 
+func getIntArray() []int {
+	x := []int{1,2,3,4,}
+	return x
+}
+
+func getStringArray() []string {
+	return []string{"a", "b", "c"}
+}
 
 func Print(text string) {
 	for _, val := range text {
 		fmt.Println(val)
 	}
+
+	for k, v :=range getStringArray() {
+		fmt.Println(k,v)
+	}
 }
 
-func Sort(arr []string){
+func StartSlice(arr []int, startIndex int) []int {
+	return arr[startIndex:]
+}
+
+func EndSlice(arr []int, endIndex int) []int{
+	return arr[:endIndex]
+}
+
+func RangeSlice(arr []int, startIndex int, endIndex int) []int {
+	return arr[startIndex:endIndex]
+}
+
+func AppendElements(arr []int, elems ...int) []int {
+	return append(arr, elems ...)
+}
+
+func AppendArray(arr []int, a []int) []int {
+	return append(arr, a...)
+}
+
+func Sort(arr []string) {
 	sort.Strings(arr)
 }
+
+func Length(arr []string) int{
+	return len(arr)
+}
+
+
